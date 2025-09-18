52bd51c915990f8d55d96a2438f24a22b9943cfc
Ranjith <d.ranjithmca@gmail.com>
2025-09-18 15:32:46 +0530

    db url changed

--- seyyone/wp-config.php
+++ seyyone/wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'seyyone_wp' );
+define( 'DB_NAME', 'seyyone_wp_dev' );
 
 /** Database username */
 define( 'DB_USER', 'dbinisystems' );
